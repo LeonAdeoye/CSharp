f94e9e22c387e522b06543c7db8516add3e4c1da
LeonAdeoye <leon.adeoye@gmail.com>
2019-09-17 22:19:03 +0800

    delegates

--- DelegateExample.cs
+++ DelegateExample.cs
@@ -27,21 +27,33 @@ namespace Development
             return myString.ToLower();
         }
 
+        public string doNothing(string myString)
+        {
+            return myString;
+        }
+
         public static void main()
         {
             // The word delegate is often confusing used to describe both the delegate type and the delegate instance.
             // But the distinction between the two is exactly the same as between any other type and instances of that type.
 
-            // STEP THREE: CREATE AN INSTANCE OF THE DELEGATE TYPE, SPECIFYING WHICH METHOD WILL BE EXECUTED WHEN THE DELEGATE INSTANCE IS INVOKED.
+
+            // STEP THREE: CREATE AN INSTANCE OF THE DELEGATE TYPE, SPECIFYING WHICH METHOD (THE ACTION) WILL BE EXECUTED WHEN THE DELEGATE INSTANCE IS INVOKED.
             StringProcessor sp1 = new StringProcessor(doSomething);
+            // When the ACTION in an instance method, you need an instance of the type.  This object instance is called the TARGET of the ACTION.
+            // When the delegate instance is invoked, the ACTION will be invoked on the TARGET.
+            // A delegate instance will prevent its target from being garbage collected if the delegate instance itself is collected.
+            // This will result in memory leaks: a long-lived object indirectly holds a reference to short-lived one, prolonging its lifetime.
             DelegateExample de = new DelegateExample();
             StringProcessor sp2 = new StringProcessor(de.doSomethingElse);
 
             // STEP FOUR: INVOKE THE DELEGATE INSTANCE
             Console.WriteLine("Invoking delegate instance sp1 returns {0}", sp1.Invoke("leon"));
-            // Invoking a delegate without using explicit invoke
+            // Invoking a delegate without using an explicit call of the Invoke method.
             Console.WriteLine("Invoking delegate instance sp2 returns {0}", sp2("LEON"));
-
+            sp2 += sp1;
+            Console.WriteLine("Invoking multicasted delegate instance sp2 returns {0}", sp2("LEON"));
+            StringProcessor sp3 = new StringProcessor(de.doSomethingElse);
         }
     }
 }
